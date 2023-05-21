3d870fe6b09497f78af077d3dc1c4d9d726f5f90
Marc Homps <homps@iut.univ-paris8.fr>
2023-05-21 17:57:23 +0200

    Modification ligne 6 Essais

--- magasin/src/Essais.java
+++ magasin/src/Essais.java
@@ -3,8 +3,8 @@ public class Essais {
 
 	public static void main(String[] args) {
 		System.out.println("Lancement du programme:");
-		Produit pdt= new Produit("Patates", 2);
-		System.out.println(pdt);
+		Produit pdterre= new Produit("Patates", 2);
+		System.out.println(pdterre);
 		Produit yaourt=new Produit("Yaourt", 3);
 		System.out.println(yaourt);
 		System.out.println("Fin du programme");
