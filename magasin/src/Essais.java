af0fabb8d4bb1c49dd1733d9410c9881dd27d366
Marc Homps <homps@iut.univ-paris8.fr>
2023-05-21 17:36:19 +0200

    Ajout de la classe Essais

---
+++ magasin/src/Essais.java
@@ -0,0 +1,10 @@
+
+public class Essais {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Produit pdt= new Produit("Patates", 2);
+		System.out.println(pdt);
+	}
+
+}
